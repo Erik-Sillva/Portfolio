2bc89c92c4eb26b37a8e9d01c3a4adae0acc1d0d
Erik Silva <104636199+Erik-Sillva@users.noreply.github.com>
2023-07-16 01:09:43 -0300

    Subindo para produção

--- src/pages/Thankyou.tsx
+++ src/pages/Thankyou.tsx
@@ -8,7 +8,7 @@ const Thankyou = () => {
             </div>
 
             <div className="p-5 text-center">
-                <h2 className='h2 text-gradient text-center'>Email Enviado</h2>
+                <h2 className='h2 text-gradient text-center'>E-mail Enviado</h2>
                 <p className='text-[20px]'>Recebemos seu e-mail. Agora, basta aguardar e ficar atento, pois iremos responder o mais rápido possível!</p>
             </div>
         </div>
